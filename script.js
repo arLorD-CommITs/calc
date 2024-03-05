04d2d7416eb1e174051f67dae64c8c0e089abd21
Christian Arlu Laderas <christianarlu@Xubuntu2024.myguest.virtualbox.org>
2024-03-06 01:17:42 +0800

    Create operate function and operator variables

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+let num1 = 5;
+let num2 = 6;
+let operators = {
+    '+': (num1, num2) => num1 + num2,
+    '-': (num1, num2) => num1 - num2,
+    '*': (num1, num2) => num1 * num2,
+    '/': (num1, num2) => num1 / num2,
+};
 
+function operate (num1, operator, num2) {
+   switch (operator) {
+    case '+':
+        return operators["+"](num1, num2);
+    case '-':
+        return operators['-'](num1, num2);
+    case '*':
+        return operators['*'](num1, num2);
+    case '/':
+        return operators['/'](num1, num2);
+   }
+}
