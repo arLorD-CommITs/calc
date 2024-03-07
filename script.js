e7792d552b5d2f83699a615503acbb6ef9e84d71
Christian Arlu Laderas <christianarlu@Xubuntu2024.myguest.virtualbox.org>
2024-03-07 11:07:39 +0800

    Start adding DOM manipulation for display and buttons

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+function operate (num1, operator, num2) {
+    switch (operator) {
+     case '+':
+         return operators["+"](num1, num2);
+     case '-':
+         return operators['-'](num1, num2);
+     case '*':
+         return operators['*'](num1, num2);
+     case '/':
+         return operators['/'](num1, num2);
+    }
+ }
+ 
+ function logHello() {
+    const text = document.createTextNode(this.textContent); 
+    console.log(text);
+    display.appendChild(text);
+     
+ }
+
 let num1 = 120;
 let num2 = 3;
 let opString = "*";
@@ -8,15 +28,10 @@ let operators = {
     '/': (num1, num2) => num1 / num2,
 };
 
-function operate (num1, operator, num2) {
-   switch (operator) {
-    case '+':
-        return operators["+"](num1, num2);
-    case '-':
-        return operators['-'](num1, num2);
-    case '*':
-        return operators['*'](num1, num2);
-    case '/':
-        return operators['/'](num1, num2);
-   }
-}
+const buttons = document.querySelectorAll('.keypad button');
+buttons.forEach((btn) => {
+    btn.addEventListener("click", logHello);
+});
+const display = document.querySelector('.display');
+
+
