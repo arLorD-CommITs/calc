de6c55a191914ce69348eabf3ba32aee18eee81b
Christian Arlu Laderas <christianarlu@Xubuntu2024.myguest.virtualbox.org>
2024-03-09 05:10:06 +0800

    Tried to optimize existing functions

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
-function operate (num1, operator, num2) {
-    switch (operator) {
+function operate (arr) {
+    switch (arr[1]) {
      case '+':
-         return operators["+"](num1, num2);
+         return operators["+"](arr[0], arr[2]);
      case '-':
-         return operators['-'](num1, num2);
+         return operators['-'](arr[0], arr[2]);
      case '*':
-         return operators['*'](num1, num2);
+         return operators['*'](arr[0], arr[2]);
      case '/':
-         return operators['/'](num1, num2);
+         return operators['/'](arr[0], arr[2]);
     }
  }
  
- function logHello() {
-    const text = document.createTextNode(this.textContent); 
-    console.log(text);
-    display.appendChild(text);
-     
- }
+ function populateDisplay() {
+    if (this.className === "clear-btn") {
+      display.textContent = "";
+    } else if (this.className === "equals-btn") {
+      
+      let displayValue = display.textContent;
+      let newDisplayValue = [];
 
-let num1 = 120;
-let num2 = 3;
-let opString = "*";
+      if (displayValue.includes("+") === true) {
+        newDisplayValue = displayValue.split("+");
+        newDisplayValue.splice(1, 0, "+");
+        console.log(newDisplayValue);
+      } else if (displayValue.includes("-") === true) {
+        newDisplayValue = displayValue.split("-");
+        newDisplayValue.splice(1, 0, "-");
+        console.log(newDisplayValue);
+      } else if (displayValue.includes("*") === true) {
+        newDisplayValue = displayValue.split("*");
+        newDisplayValue.splice(1, 0, "*");
+        console.log(newDisplayValue);
+      } else if (displayValue.includes("/") === true) {
+        newDisplayValue = displayValue.split("/");
+        newDisplayValue.splice(1, 0, "/");
+        console.log(newDisplayValue);
+      } 
+
+
+
+      display.textContent = operate(newDisplayValue);
+    } 
+    else {
+        
+      display.textContent += this.textContent;
+    }
+  }
+
 let operators = {
     '+': (num1, num2) => num1 + num2,
     '-': (num1, num2) => num1 - num2,
@@ -30,7 +56,7 @@ let operators = {
 
 const buttons = document.querySelectorAll('.keypad button');
 buttons.forEach((btn) => {
-    btn.addEventListener("click", logHello);
+    btn.addEventListener("click", populateDisplay);
 });
 const display = document.querySelector('.display');
 
